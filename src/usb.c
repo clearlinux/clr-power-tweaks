83a86d5d3039087ea9d776650a702dfd52066bb3
Arjan van de Ven <arjan@linux.intel.com>
2016-06-26 03:06:22 +0000

    add bdw hub

--- src/usb.c
+++ src/usb.c
@@ -49,6 +49,7 @@ static struct usb_dev known_good[] =
 { 
 /* {0x8087, 0x09fa, 0x0000} */ /* Intel Sensor Hub. Bust on USB autosusped */
 {0x8087, 0x07dc, 0x0000},
+{0x8087, 0x8001, 0x0009},	/* EHCI/XHCI built in hub */
 /* BYT-T additions from T100 - need to verify they are all BYT-T systems */
 {0x0b05, 0x17e0, 0x0000},
 {0x0b95, 0x7720, 0x00ff},
