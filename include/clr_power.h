d50eda22c314a60ac29fbfbc6bb7a47d282c2a72
Joe Konno <joe.konno@intel.com>
2019-10-22 14:41:08 -0700

    clr_power.h: prune unused macros
    Signed-off-by: Joe Konno <joe.konno@intel.com>

--- include/clr_power.h
+++ include/clr_power.h
@@ -14,9 +14,6 @@ struct usb_dev {
 	unsigned int class;
 };
 
-#define CLASS(x) {0,0,x},
-#define DEVICE(v,d) {v,d,0},
-
 extern void lib_init(const bool dbg);
 extern int write_string_to_file(const char *filename, const char *string);
 extern int write_string_to_files(const char *glob, const char *string);
