c9d22af0b42be5c3f1134d9db133f81933810267
Arjan van de Ven <arjan@linux.intel.com>
2023-12-28 17:43:19 +0000

    network tuning

--- src/data.h
+++ src/data.h
@@ -100,6 +100,9 @@ struct write_struct write_list[] = {
 	{"/proc/sys/net/ipv4/conf/default/secure_redirects", "0", 0},
 
 	{"/proc/sys/net/ipv4/tcp_slow_start_after_idle", "0", 0},
+	{"/proc/sys/net/core/busy_poll", "50", 0},
+	{"/proc/sys/net/core/busy_read", "50", 0},
+
 
 	// SATA link power management
 	{"/sys/class/scsi_host/*/link_power_management_policy", "med_power_with_dipm", 0},
