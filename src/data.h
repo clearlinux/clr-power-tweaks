05f6c3309199f34b66825c3d5f09d0935da898fb
Arjan van de Ven <arjan@linux.intel.com>
2025-01-03 16:04:22 +0000

    also set EPP

--- src/data.h
+++ src/data.h
@@ -134,6 +134,7 @@ struct write_struct write_list[] = {
 	{"/sys/devices/system/cpu/microcode/reload", "1", 0},
 	{"/sys/devices/system/cpu/cpu0/power/energy_perf_bias", "balance-performance", 0}, 
 	{"/sys/devices/system/cpu/cpu*/power/energy_perf_bias", "balance-performance", 0}, 
+	{"/sys/devices/system/cpu/cpu*/power/energy_perf_bias", "balance_performance", 0}, 
 
 	{"/proc/sys/kernel/nmi_watchdog", "0", 0},
 	{"/sys/block/{sd,mmc,nvme, 0}*/queue/iosched/slice_idle", "0"},
